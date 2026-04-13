db97288e8d2fdaaf3874efb66757d5cbea1bae83
Kevin Huynh <kevin.huynh.4414@gmail.com>
2026-04-13 12:29:14 -0400

    Create Final project

---
+++ FinalProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinalProject
+//
+//  Created by Kevin Huynh on 4/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Kevin Huynh on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
